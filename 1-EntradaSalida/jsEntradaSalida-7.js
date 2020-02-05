88e96605d515c434fda77c10f8233c3715d5a68b
joacomarquillas <60696458+joacomarquillas@users.noreply.github.com>
2020-02-05 10:24:26 -0300

    ejercicio 7 suma

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,8 +3,22 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
-	
+{
+
+	var primerNumero; 
+	var segundoNumero;
+	var resultado;
+
+	primerNumero=document.getElementById('numeroUno').value;
+	primerNumero=parseInt(primerNumero)
+
+	segundoNumero=document.getElementById('numeroDos').value;
+	segundoNumero=parseInt(segundoNumero)
+
+
+	resultado= primerNumero + segundoNumero; 
+
+	console.log(resultado);
 }
 
 function restar()

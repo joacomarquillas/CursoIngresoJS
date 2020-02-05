b2314db10f3e5b1f209c37be7aa0ed6031254af1
joacomarquillas <60696458+joacomarquillas@users.noreply.github.com>
2020-02-05 11:32:59 -0300

    ejercicio 9 resuelto

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -5,5 +5,17 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
-	
+	var sueldo;
+	var resultado;
+	sueldo=document.getElementById('sueldo').value;
+	sueldo=parseInt(sueldo);
+
+	resultado=sueldo * 1.10;
+
+	document.getElementById('resultado').value = resultado;
+
+
+	console.log("el importe con aumento es : " + resultado);
+
+
 }
